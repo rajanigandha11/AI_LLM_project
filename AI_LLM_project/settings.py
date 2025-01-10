9a04ae6dbd4e64c6b79824c0b023541fcb8b98b6
rajanigandha jadhav <59056709+rajanigandha11@users.noreply.github.com>
2025-01-10 09:48:44 +0530

    Update settings.py

--- AI_LLM_project/settings.py
+++ AI_LLM_project/settings.py
@@ -141,12 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AWS_ACCESS_KEY_ID =  'AKIAS2VS4AYUN46FZBGR'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'newdb1'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'Asia Pacific (Mumbai) ap-south-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
